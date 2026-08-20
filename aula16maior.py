d3a86e066634f51b170462a00e3031894bdbea6b
DaviCruz1608 <cruzcdavi16@gmail.com>
2026-08-19 23:10:06 -0300

    Adiciona exercício de condicionais
    Adiciona exercício de condicionais

---
+++ aula16maior.py
@@ -0,0 +1,9 @@
+temperatura = int(input('digite a temperatura.'))
+if temperatura >= 30:
+    print('Está muito quente.')
+elif temperatura >= 20:
+    print('Temperatura agradavel')
+else:
+    print('Está frio.')
+
+
